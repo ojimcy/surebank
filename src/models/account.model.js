10c67da908db450cb8efdcd5c7584aa90defca43
ojimcy <ojimcy247@gmail.com>
2023-06-30 00:36:31 +0100

    account

---
+++ src/models/account.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const accountSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    accountNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    availableBalance: {
+      type: Number,
+      required: true,
+    },
+    ledgerBalance: {
+      type: Number,
+      required: true,
+    },
+    accountType: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    accountManagerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    branchId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Branch',
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'active',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+accountSchema.plugin(toJSON);
+accountSchema.plugin(paginate);
+
+/**
+ * @typedef Account
+ */
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = Account;
