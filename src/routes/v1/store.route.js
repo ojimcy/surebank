6d31f605708cd89e97cd0a923062649ea3b7bf45
ojimcy <ojimcy247@gmail.com>
2024-01-05 09:15:36 +0100

    brands

--- src/routes/v1/store.route.js
+++ src/routes/v1/store.route.js
@@ -3,17 +3,24 @@ const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const storeValidation = require('../../validations/store.validation');
 const storeController = require('../../controllers/store.controller');
+const brandValidation = require('../../validations/brand.validation');
+const brandController = require('../../controllers/brand.controller');
 
 const router = express.Router();
+
+router
+  .route('/brands')
+  .post(auth('manageBrand'), validate(brandValidation.createBrand), brandController.createBrand)
+  .get(auth('manageBrand'), validate(brandValidation.viewBrands), brandController.viewBrands);
+
+router
+  .route('/brands/:brandId')
+  .patch(auth('manageBrand'), validate(brandValidation.updateBrand), brandController.updateBrand)
+  .delete(auth('manageBrand'), validate(brandValidation.deleteBrand), brandController.deleteBrand);
 
 router
   .route('/categories')
   .post(auth('manageCategory'), validate(storeValidation.createCategory), storeController.createCategory)
   .get(storeController.getCategories);
-
-router
-  .route('/brands')
-  .post(auth('manageBrands'), validate(storeValidation.createCategory), storeController.createBrand)
-  .get(storeController.getBrands);
 
 module.exports = router;
